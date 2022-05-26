1aeaea4c146df34da104ffa2f174456fc5fd9161
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-05-26 16:12:42 +0700

    Add Tracking Vaksin ASP.NET Core Web MVC

---
+++ Tracking Vaksin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tracking_Vaksin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
