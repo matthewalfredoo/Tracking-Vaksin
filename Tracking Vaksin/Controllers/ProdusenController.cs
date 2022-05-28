502b0bde473f560629488be05220aff6113b10ff
Joshua Pratama Silitonga <joshuasilitonga59@gmail.com>
2022-05-29 00:06:42 +0700

    login

---
+++ Tracking Vaksin/Controllers/ProdusenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tracking_Vaksin.Controllers
+{
+    public class ProdusenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
