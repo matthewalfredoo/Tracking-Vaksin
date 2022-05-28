2cd241b1dfaa6527b85ef7a01255329edd073b42
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-05-28 21:08:13 +0700

    Connected to services

--- Tracking Vaksin/Controllers/HomeController.cs
+++ Tracking Vaksin/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ namespace Tracking_Vaksin.Controllers
 {
     public class HomeController : Controller
     {
+        private int StatusCode = 400;
+        private string Message = "";
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,7 +18,15 @@ namespace Tracking_Vaksin.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            Tracking_Vaksin_Services.BPOMS bpomS = null;
+            string username = "bpom1";
+            string password = "12345678";
+
+            ServiceModelBPOM.ServiceModelBPOMClient serviceModelBPOMClient = new ServiceModelBPOM.ServiceModelBPOMClient();
+            serviceModelBPOMClient.login(ref bpomS, username, password, ref StatusCode, ref Message);
+
+            ServiceModelBPOM.loginRequest LoginRequest = new ServiceModelBPOM.loginRequest(bpomS, username, password, StatusCode, Message);
+            return View(LoginRequest);
         }
 
         public IActionResult Privacy()
